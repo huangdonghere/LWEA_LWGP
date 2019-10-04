f0934a952098ed2e2a7ff5feb78b968723cffb67
huangdonghere <51360835+huangdonghere@users.noreply.github.com>
2019-10-04 14:52:27 +0800

    Add files via upload

---
+++ getAllSegs.m
@@ -0,0 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                   %
+% This is a demo for the LWEA and LWGP algorithms. If you find this %
+% code useful for your research, please cite the paper below.       %
+%                                                                   %
+% Dong Huang, Chang-Dong Wang, and Jian-Huang Lai.                  %
+% "Locally weighted ensemble clustering."                           %
+% IEEE Transactions on Cybernetics, to appear, 2017.                %
+%                                                                   %
+% The code has been tested in Matlab R2014a and Matlab R2015a on a  %
+% workstation with Windows Server 2008 R2 64-bit.                   %
+%                                                                   %
+% https://www.researchgate.net/publication/316681928                %
+%                                                                   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [bcs, baseClsSegs] = getAllSegs(baseCls)
+
+[N,M] = size(baseCls);
+% n:    the number of data points.
+% M:    the number of base clusterings.
+% nCls:     the number of clusters (in all base clusterings).
+
+bcs = baseCls;
+nClsOrig = max(bcs,[],1);
+C = cumsum(nClsOrig); 
+bcs = bsxfun(@plus, bcs,[0 C(1:end-1)]);
+nCls = nClsOrig(end)+C(end-1);
+baseClsSegs=sparse(bcs(:),repmat([1:N]',1,M), 1,nCls,N); 
